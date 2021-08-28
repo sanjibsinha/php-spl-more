b8234e2aec2cd162c294f7110e0e57963c232ed5
Sanjib Sinha <sanjib12sinha@gmail.com>
2021-08-28 07:02:14 +0530

    more examples

--- chapter-six/arrayIterator.php
+++ chapter-six/arrayIterator.php
@@ -43,6 +43,39 @@
       </h2> 
       <div class="container">
         <code>
+
+        <?php
+          $books = array(
+              "PHP" => "PHP 8 Standard PHP Library",
+              "WordPress" => "WordPress Coding Made Easy",
+              "Flutter" => "Flutter: A Complete Guide",
+              "Dart" => "Beginning Flutter with Dart"
+          );
+          $obj = new ArrayObject( $books );
+          $iterator = $obj->getIterator();
+
+          // We can count how many items are we iterating over?
+
+          echo "Iterating over: " . $obj->count() . " values</br></br>";
+
+          echo "<h1>Getting output from While Loop with Key=Value</h1>";
+
+          // Iterate over the values in the ArrayObject:
+          while( $iterator->valid() )
+          {
+              echo $iterator->key() . " = " . $iterator->current() . "</br></br>";
+              $iterator->next();
+          }
+
+          echo "</br></br>";
+          echo "<h1>Getting output from forach Loop, only value </h1>";
+
+          // The good thing here is that it can be iterated with foreach loop
+
+          foreach ($iterator as $key=>$val)
+          echo $key.":".$val."</br></br>";
+
+        ?>
           
         </code>
       </div>
